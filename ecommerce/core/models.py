9f2c3d9d1bcdbe01b91b008295ec2a86674af625
Romeo Routaboul <romeo13mil@gmail.com>
2023-06-30 16:18:51 -0300

    incorporación de modelos y cambio en settings.py

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    nombre_completo = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=80)
+
+class Categoria(models.Model):
+    descripcion = models.CharField(max_length=100)
+
+class Item(models.Moddel):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=300)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
