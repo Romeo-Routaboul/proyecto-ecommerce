f586ceb1d7ba078861c57454b4e7b1272bd0f52e
Romeo Routaboul <romeo13mil@gmail.com>
2023-07-17 16:18:30 -0300

    implementación de buscador ("core/buscador")

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -4,5 +4,8 @@ from core.views import*
 urlpatterns = [
 #path("", inicio, name="inicio"),
 path("signup/", SignUpView.as_view(), name="signup"),
+path("inicio/", inicio, name="inicio"),
+path("buscar/", buscar, name="buscar"),
+path("resultado/", resultado_busqueda_productos, name="resultado"),
 
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from core.models import*
+from core.forms import*
 
 from .forms import MyUserCreationForm
 
@@ -13,3 +15,15 @@ class SignUpView(CreateView):
     form_class = MyUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def inicio(request):
+    return render (request, "core/index.html")
+
+def buscar(request):
+    return render (request, "core/buscador.html")
+
+def resultado_busqueda_productos(request):
+    nombre_item = request.GET["nombre_item"]
+
+    resultados = Item.objects.filter(nombre__icontains = nombre_item)
+    return render (request, "core/resultado-busqueda.html", {"resultados": resultados})
